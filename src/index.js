7128c61b4a285d554a890eadcb1e4cddb7a8e645
Mateus Ferreira <mateushenrique123@live.com>
2022-12-21 20:50:23 -0300

    ajustes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ const routes = require("./routes/indexRouter.js");
 const app = express();
 const cors = require("cors");
 
-sequelize.sync().then(() => console.log("dataBase connected successfull!"));
+sequelize.sync().then(() => console.log("dataBase connected successfull!"))
 app.use(cors());
+app.use(express.json());
+
 app.use("/api/usuarios", routes);
 app.use("/api", routes);
 app.use("/api", routes);
+
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 })
