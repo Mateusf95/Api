db5db14acd79ddb6bd42e82801da5c977aa24801
Mateus Ferreira <mateushenrique123@live.com>
2022-12-20 22:42:11 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const sequelize = require("./config/database.js");
 const routes = require("./routes/indexRouter.js");
 const app = express();
+const cors = require("cors");
 
+sequelize.sync().then(() => console.log("dataBase connected successfull!"));
 app.use("/api/usuarios", routes);
 app.use("/api", routes);
 app.use("/api", routes);
